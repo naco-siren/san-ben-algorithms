6e90560b894583a29565398691e2e8355f57fbae
Han Yin <naco.siren.2012@gmail.com>
2017-09-18 11:08:31 -0500

    Added LC 253 with 2 solutions

---
+++ src/base/Interval.java
@@ -0,0 +1,11 @@
+package base;
+
+/**
+ * Created by naco_siren on 9/18/17.
+ */
+public class Interval {
+    public int start;
+    public int end;
+    public Interval() { start = 0; end = 0; }
+    public Interval(int s, int e) { start = s; end = e; }
+}
