9810e291995f530ed9069d526252eca66865c881
Han Yin <naco.siren.2012@gmail.com>
2017-09-17 20:48:19 -0500

    Fixed LC 5; Added LC 251

--- src/leetcode/LC_5.java
+++ src/leetcode/LC_5.java
@@ -8,7 +8,7 @@ public class LC_5 {
     public static void main(String[] args) {
         LC_5 lc = new LC_5();
 
-        lc.longestPalindrome("babab");
+        String r1 = lc.longestPalindrome("cbabab");
         return;
     }
 
@@ -21,10 +21,10 @@ public class LC_5 {
         int l, r;
         for (int c = 1; c <= 2 * len - 1; c++) {
             for (l = (c - 1) / 2, r = c / 2; l >= 0 && r < len && s.charAt(l) == s.charAt(r); l--, r++);
-            int curLen = r - l + 1;
+            int curLen = r - l - 1;
             if (curLen > maxLen) {
                 maxLen = curLen;
-                start = l;
+                start = l + 1;
             }
         }
         return s.substring(start, start + maxLen);
