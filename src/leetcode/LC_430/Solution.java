9ee94592302f8877e4ad5ee4dae90107e98a33c5
Han Yin <naco.siren.2012@gmail.com>
2020-03-22 20:31:43 -0700

    Added iteration solution to Leetcode 430. Flatten a Multilevel Doubly Linked List

--- src/leetcode/LC_430/Solution.java
+++ src/leetcode/LC_430/Solution.java
@@ -1,9 +1,43 @@
 package leetcode.LC_430;
 
+import java.util.Stack;
+
 /**
  * 430. Flatten a Multilevel Doubly Linked List
  */
 class Solution {
+    public NodeWithChild flattenIteration(NodeWithChild head) {
+        if (head == null)
+            return null;
+
+        // Initialize a stack
+        Stack<NodeWithChild> stack = new Stack<>();
+        stack.push(head);
+
+        NodeWithChild pre = null;
+        NodeWithChild current;
+        while (!stack.isEmpty()) {
+            current = stack.pop();
+
+            // Push `next` first so that `child` gets popped first
+            if (current.next != null)
+                stack.push(current.next);
+            if (current.child != null)
+                stack.push(current.child);
+
+            // Concatenate double-linked list
+            if (pre != null) {
+                pre.next = current;
+                current.prev = pre;
+            }
+
+            current.child = null;
+            pre = current;
+        }
+
+        return head;
+    }
+
     public NodeWithChild flatten(NodeWithChild head) {
         NodeWithChild sentinel = new NodeWithChild();
         sentinel.next = head;
