0f675d4a9bdaff7261bb38f61cdbbdd95697e4b7
Han Yin <naco.siren.2012@gmail.com>
2020-05-17 14:16:11 -0700

    Format and document Leetcode 31. Next Permutation

---
+++ src/leetcode/LC_031/Solution31.java
@@ -0,0 +1,53 @@
+package leetcode.LC_031;
+
+
+/**
+ * 31. Next Permutation
+ */
+class Solution31 {
+
+    void nextPermutation(int[] nums) {
+        // Find the longest descending subarray starting from the right end.
+        // e.g. [1,2,5,4,3]  ->  s = 2
+        //
+        int s = nums.length - 1;
+        while (s > 0 && nums[s-1] >= nums[s])
+            s--;
+
+        // Early return if the whole array is descending
+        if (s == 0 || nums.length < 2) {
+            reverse(nums, 0);
+            return;
+        }
+
+        // Find the smallest number in this subarray yet larger than the one right before the subarray, i.e. `ceiling`,
+        // as we DO NOT want to modify the digits before `s`, that would change the value in a much larger scale.
+        //
+        // And apparently it must exist, because `nums[s] < nums[s+1]` is the condition for breaking the loop!
+        final int source = nums[s-1];
+        int ceiling = Integer.MAX_VALUE, ceilingIdx = -1;
+        for (int i = s; i < nums.length; i++) {
+            if (nums[i] <= source)
+                continue;
+
+            if (nums[i] <= ceiling) {
+                ceiling = nums[i];
+                ceilingIdx = i;
+            }
+        }
+
+        // Swap these two numbers and reverse the subarray.
+        nums[s-1] = ceiling;
+        nums[ceilingIdx] = source;
+
+        reverse(nums, s);
+    }
+
+    void reverse(final int[] nums, final int start) {
+        for (int l = start, r = nums.length - 1; l < r; l++, r--) {
+            final int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+        }
+    }
+}
--- src/leetcode/LC_031/Solution31Test.java
+++ src/leetcode/LC_031/Solution31Test.java
@@ -4,40 +4,47 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class SolutionTest {
+public class Solution31Test {
 
     @Test
     public void nextPermutation_0() {
         final int[] arr = new int[]{};
-        Solution.nextPermutation(arr);
+        new Solution31().nextPermutation(arr);
         assertArrayEquals(new int[]{}, arr);
     }
 
     @Test
     public void nextPermutation_1() {
         final int[] arr = new int[]{1,2,3};
-        Solution.nextPermutation(arr);
+        new Solution31().nextPermutation(arr);
         assertArrayEquals(new int[]{1,3,2}, arr);
     }
 
     @Test
     public void nextPermutation_2() {
         final int[] arr = new int[]{1,1,5};
-        Solution.nextPermutation(arr);
+        new Solution31().nextPermutation(arr);
         assertArrayEquals(new int[]{1,5,1}, arr);
     }
 
     @Test
     public void nextPermutation_3() {
         final int[] arr = new int[]{3,2,1};
-        Solution.nextPermutation(arr);
+        new Solution31().nextPermutation(arr);
         assertArrayEquals(new int[]{1,2,3}, arr);
     }
 
     @Test
     public void nextPermutation_4() {
         final int[] arr = new int[]{3,4,4,3,3};
-        Solution.nextPermutation(arr);
+        new Solution31().nextPermutation(arr);
         assertArrayEquals(new int[]{4,3,3,3,4}, arr);
+    }
+
+    @Test
+    public void nextPermutation_5() {
+        final int[] arr = new int[]{1,2,5,4,3};
+        new Solution31().nextPermutation(arr);
+        assertArrayEquals(new int[]{1,3,2,4,5}, arr);
     }
 }
