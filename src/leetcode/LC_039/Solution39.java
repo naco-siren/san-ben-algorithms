fc8a90f26db07de0e42c4f85956c0e13b4d4fdb7
Han Yin <naco.siren.2012@gmail.com>
2020-05-16 17:20:59 -0700

    Optimize Leetcode 39. Combination Sum

--- src/leetcode/LC_039/Solution39.java
+++ src/leetcode/LC_039/Solution39.java
@@ -1,6 +1,7 @@
 package leetcode.LC_039;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.TreeSet;
 
@@ -9,6 +10,34 @@ import java.util.TreeSet;
  */
 class Solution39 {
     List<List<Integer>> combinationSum(int[] candidates, int target) {
+        // Require candidates array to be ascending
+        Arrays.sort(candidates);
+
+        // Perform a DFS
+        List<List<Integer>> results = new ArrayList<>();
+        search(candidates, target, 0, new ArrayList<>(), results);
+        return results;
+    }
+
+    private void search(int[] candidates, final int target, final int index, List<Integer> prefix, List<List<Integer>> results) {
+        if (target == 0) {
+            results.add(new ArrayList<>(prefix));
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            final int cand = candidates[i];
+            if (cand > target)
+                continue;
+
+            prefix.add(cand);
+            search(candidates, target - cand, i, prefix, results);
+            prefix.remove(prefix.size() - 1);
+        }
+    }
+
+
+    List<List<Integer>> combinationSumTreeMap(int[] candidates, int target) {
         // Maintain a TreeMap for candidates
         TreeSet<Integer> cands = new TreeSet<>();
         for (int cand : candidates)
@@ -16,11 +45,11 @@ class Solution39 {
 
         // Preform a DFS
         List<List<Integer>> results = new ArrayList<>();
-        search(cands, target, new ArrayList<>(), results);
+        searchTreeMap(cands, target, new ArrayList<>(), results);
         return results;
     }
 
-    private void search(TreeSet<Integer> cands, final int target, List<Integer> prefix, List<List<Integer>> results) {
+    private void searchTreeMap(TreeSet<Integer> cands, final int target, List<Integer> prefix, List<List<Integer>> results) {
         if (target == 0) {
             results.add(new ArrayList<>(prefix));
             return;
@@ -30,7 +59,7 @@ class Solution39 {
                 continue;
 
             prefix.add(cand);
-            search(cands, target - cand, prefix, results);
+            searchTreeMap(cands, target - cand, prefix, results);
             prefix.remove(prefix.size() - 1);
         }
     }
